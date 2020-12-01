80e4910acae7fd13686a67f325b69f1ec8508d7e
rubeniskov <me@rubeniskov.com>
2020-12-01 14:28:26 +0100

    refactor: packages names

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
 
+const { create } = require('ipfs');
 const server = require('@cuser/server');
 
-server({
-  secret: '',
+const node = create({
+  EXPERIMENTAL: {
+    ipnsPubsub: true,
+  }
+});
+
+server(node, {
+  secret: process.env.SECRET || '',
   port: process.env.PORT || 3000,
+  host: process.env.HOST || '0.0.0.0',
   cors: process.argv.includes('--cors') || false,
 });
--- packages/server/index.js
+++ packages/server/index.js
@@ -1,37 +1,65 @@
 const debug = require('debug')('cuser:server');
-const bodyParser = require('body-parser');
 const express = require('express');
-const cors = require('cors');
-const authMiddleware = require('@cuser/server-middleware-auth');
-const apollo = require('@cuser/server-middleware-graphql');
-const rest = require('@cuser/server-middleware-rest');
-const cuser = require('@cuser/core')();
 
-const server = (opts = {}) => {
+const defaults = {
+  cors: false,
+  auth: '/auth',
+  rest: '/rest',
+  grapqhl: '/gql',
+}
+
+const grpath = (rpath, fallback) => typeof auth === 'string' ? rpath : fallback;
+
+const server = (node, opts) => {
+  const {
+    cors,
+    auth,
+    rest,
+    grapqhl,
+  } = { ...defaults, ...opts };
+
   const app = express();
-  const authRouter = authMiddleware(cuser)
-  if(opts.cors) {
-    app.use(cors());
+  const cuser = opts.cuser = require('@cuser/core')(node, opts);
+
+  if(cors) {
+    app.use(require('cors'));
   }
-  app.use('/auth', authRouter.authenticate);
-  app.use(authRouter);
-  app.use('/gql', apollo(cuser));
-  app.use(rest(cuser));
 
-  app.listen(opts.port, () => {
-    debug('listening %s:%s', '0.0.0.0', opts.port);
-  });
+  if (auth) {
+    const authMiddleware = require('@cuser/express-middleware-auth');
+    const authRouter = authMiddleware(cuser);
+    app.use(grpath(auth, defaults.auth), authRouter.authenticate);
+    app.use(authRouter);
+  }
 
-  app.use(bodyParser.json(), function (err, _, res, __) {
+  if (rest) {
+    const restMiddleware = require('@cuser/express-middleware-rest');
+    app.use(grpath(restMiddleware, defaults.rest), restMiddleware(cuser));
+  }
+
+  if (grapqhl) {
+    const graphqlMiddleware = require('@cuser/express-middleware-graphql');
+    app.use(grpath(grapqhl, defaults.grapqhl), graphqlMiddleware(cuser));
+  }
+
+  // Error handling
+  app.use(require('body-parser'), function (err, _, res, __) {
     res.json({
       ...err,
       message: err.message,
     });
   });
+
+  if (opts.port && opts.host) {
+    const hostname = [opts.host, opts.port].filter(Boolean).join(':');
+    app.listen(hostname, () => {
+      debug('listening %s', hostname);
+    });
+  }
 }
 
-
 module.exports = server;
+module.exports.defaults = defaults;
 
 if (require.main === module) {
   server({ port: process.env.PORT || 3000 });
